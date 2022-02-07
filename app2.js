5fe8b94b0717feb311d9af21ae51b290adcc26de
Jorge <93343250+jsuazof@users.noreply.github.com>
2022-02-07 12:07:57 -0300

    Final
    Final

--- app2.js
+++ app2.js
@@ -6,21 +6,49 @@ class Paciente {
         this.diagnostico = diagnostico;
         
     }
+
+get nombre() {
+    return this._nombre
+}
+get edad() {
+    return this._edad
+}
+get rut() {
+    return this._rut
+}
+get diagnostico() {
+    return this._diagnostico
+}
+set nombre(value) {
+    this._nombre = value
+}
+set edad(value) {
+    this._edad = value
+}
+set rut(value) {
+    this._rut = value
+}
+set diagnostico(value) {
+    this._diagnostico = value
+}
 }    
 class Consultorio {
         constructor(nombre){
             this.nombre = nombre;
-            this.pacientes = [];
+            this._pacientes = [];
         }
 
     get getPacientes() {
         return this.pacientes;
     }
 
-    set setPacientes(paciente){
-        this.pacientes.push(paciente);
+    set pacientes(value) {
+        this._pacientes.push(value)
     }
-   
+
+    agregarPaciente(value) {
+        this._pacientes.push(value)
+    }  
 
 };
 
@@ -43,8 +71,14 @@ const jorge = new Paciente ("Jorge", 49, "11.111.111-1", "Tos");
 const miguel = new Paciente ("Miguel", 25, "13.333.333-3", "Sed");
 const claudio = new Paciente ("Claudio", 53, "14.444.444-4", "Torticolis" )
 
-quinta.setConsultorio = jorge;
-quinta.setConsultorio = miguel;
-quinta.setConsultorio = claudio;
 
 console.log(jorge);
+quinta.agregarPaciente(miguel);
+console.log(quinta);
+
+
+
+
+
+
+
